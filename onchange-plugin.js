0071169adca542515542885ca8f5c6046f8a2a3a
Michael Swain <michael.swain@dot.state.fl.us>
2013-05-17 15:58:01 -0400

    on change plugin
    plugin sample for an event only plugin

---
+++ onchange-plugin.js
@@ -0,0 +1,22 @@
+﻿/* http://github.com/mindmup/bootstrap-wysiwyg */
+/*global jQuery, $*/
+/*jslint browser:true*/
+(function ($, undef) {
+    'use strict';
+
+    function initializer(editor, toolbar, options) {
+        // dirty hack for now
+        editor.on("input DOMNodeInserted DOMNodeRemoved DOMCharacterDataModified", function (e) { options.changeCallback(editor); });
+
+        // return no object since this plugin has no toolbar option
+        return undef;
+    }
+
+    $.fn.wysiwyg.defaults = $.extend($.fn.wysiwyg.defaults, {
+        changeCallback: function (editor) { editor.trigger("change"); }
+    });
+    $.fn.wysiwyg.plugins = $.extend($.fn.wysiwyg.plugins, {
+        "event.change": initializer
+    });
+
+} (window.jQuery));
